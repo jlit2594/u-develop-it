74a951e50f2a18dd48fb211ef6c3bd4ab3f191e5
J Litt <jdlitton2594@gmail.com>
2022-07-19 17:59:18 -0500

    12.1

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    lane_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL
+);
